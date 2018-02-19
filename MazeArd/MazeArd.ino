2cdbc13bcc74beecfda537488fce395eb103ccc1
UNicoTF <nico.taglia1572000@gmail.com>
2018-02-19 16:49:54 +0100

    Matrix.h for STM

--- MazeArd/MazeArd.ino
+++ MazeArd/MazeArd.ino
@@ -1,10 +1,24 @@
 #include "Matrix.h"
+Matrix mat;
+bool info[2];
+
+#define Victim info[1]
+#define Black info[0]
+
 void setup() {
   Serial.begin(115200);
-  Matrix mat;
+  mat.init();
   int arr[] = {10,20,30};
-  mat._check(arr, 18, 14, 22);
+  mat.check(arr, 18, 14, 22);
+  mat.move(1);
+  mat.move(0);
+  Serial.println(mat.end());
 }
 
-void loop(){
+void loop(){  
+  mat.getInfo(info);
+  Serial.print(Victim);
+  Serial.print(' ');
+  Serial.println(Black);
+  Serial.println(mat.getDir());
 }
